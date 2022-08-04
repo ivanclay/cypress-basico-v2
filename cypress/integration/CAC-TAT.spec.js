999d5c0becbd37a7212b0ba163a2d6f10d7b1e58
ivanclay <ivan.clay.moura@gmail.com>
2022-08-04 09:32:54 -0300

    feat: seção 05

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -92,4 +92,34 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible');
     })
 
+    it('seleciona um produto (YouTube) por seu texto (USE DOM ELEM)', function() {
+        cy.get('select').select('YouTube').should('have.value', 'youtube');
+        
+    })
+
+    it('seleciona um produto (YouTube) por seu texto (USE ELEM ID)', function() {
+        cy.get('#product').select('YouTube').should('have.value', 'youtube');
+        
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value) (USE DOM ELEM)', function() {
+        cy.get('select').select('mentoria').should('have.value', 'mentoria');
+        
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)  (USE ELEM ID)', function() {
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria');
+        
+    })
+
+    it('seleciona um produto (Blog) por seu índice (USE DOM ELEM)', function() {
+        cy.get('select').select(1).should('have.value', 'blog');
+        
+    })
+
+    it('seleciona um produto (Blog) por seu índice  (USE DOM ELEM)', function() {
+        cy.get('#product').select(1).should('have.value', 'blog');
+        
+    })
+
   })
